b13ec5c3c328c7a75674df23fccd99f0bab670e2
cyl <2893176150@qq.com>
2025-03-05 16:07:20 +0800

    feat: :sparkles: invoice form add validate

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,28 +7,48 @@ import { redirect } from "next/navigation";
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(), // 强制类型转换为number
-  status: z.enum(["pending", "paid"]),
+  customerId: z.string({
+    invalid_type_error: "请选择一个用户",
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than $0." }), // 强制类型转换为number
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "请选择发票状态",
+  }),
   date: z.string(),
 });
 // 创建发票 不需要id和date 字段，omit删除
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+export async function createInvoice(prevState: State, formData: FormData) {
+  // 使用zod 验证表单
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice.",
+    };
+  }
+  const { customerId, amount, status } = validatedFields.data;
   // 金额转换为分
   const amountInCents = amount * 100;
   // 创建一个新的格式为 "YYYY-MM-DD" 的日期
   const date = new Date().toISOString().split("T")[0];
-  const rawFormData = {
-    customerId: formData.get("customerId"),
-    amount: formData.get("amount"),
-    status: formData.get("status"),
-  };
+
   console.log("创建发票", customerId, status, amountInCents, date);
   try {
     await sql`
@@ -49,13 +69,26 @@ export async function createInvoice(formData: FormData) {
 }
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData
+) {
+  // 1、验证表单
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
-
+  // 2、未成功 返回错误
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Update Invoice.",
+    };
+  }
+  // 3、获取表单数据
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
@@ -74,8 +107,6 @@ export async function updateInvoice(id: string, formData: FormData) {
   redirect("/dashboard/invoices");
 }
 export async function deleteInvoice(id: string) {
-  // TODO test
-  throw new Error("Failed to Delete Invoice");
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
