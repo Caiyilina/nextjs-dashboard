04f14547fd5011a14c8060439542ef35163a9521
cyl <2893176150@qq.com>
2025-03-05 17:45:22 +0800

    feat: :sparkles: customer page
    用户页面完善

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,26 @@
+import { lusitana } from "@/app/ui/fonts";
+import Search from "@/app/ui/search";
+import { CustomersTableSkeleton } from "@/app/ui/skeletons";
+import Table from "@/app/ui/customers/table";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    query?: string | undefined;
+  }>;
+}) {
+  const { query = "" } = await searchParams;
+  return (
+    <div className="w-full">
+      <Suspense key={query} fallback={<CustomersTableSkeleton />}>
+        <Table query={query} />
+      </Suspense>
+    </div>
+  );
 }
